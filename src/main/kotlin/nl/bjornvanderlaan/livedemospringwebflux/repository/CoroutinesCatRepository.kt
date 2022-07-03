f4e7829cf3e34b0c1d60e7b09ef674236939498d
Bjorn van der Laan <bjorn.vdlaan@gmail.com>
2022-07-03 15:45:27 +0200

    Added annotation to repository

--- src/main/kotlin/nl/bjornvanderlaan/livedemospringwebflux/repository/CoroutinesCatRepository.kt
+++ src/main/kotlin/nl/bjornvanderlaan/livedemospringwebflux/repository/CoroutinesCatRepository.kt
@@ -3,8 +3,9 @@ package nl.bjornvanderlaan.livedemospringwebflux.repository
 import kotlinx.coroutines.flow.Flow
 import nl.bjornvanderlaan.livedemospringwebflux.model.Cat
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.stereotype.Repository
 
-
+@Repository
 interface CoroutinesCatRepository : CoroutineCrudRepository<Cat, Long> {
     override fun findAll(): Flow<Cat>
     override suspend fun findById(id: Long): Cat?
