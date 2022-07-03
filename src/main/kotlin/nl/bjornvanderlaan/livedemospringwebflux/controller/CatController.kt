6ddcb16321da75d21c356e380141ef39d3d3d26b
Bjorn van der Laan <bjorn.vdlaan@gmail.com>
2022-07-03 16:02:04 +0200

    Updated controller to create checkpoint 4

--- src/main/kotlin/nl/bjornvanderlaan/livedemospringwebflux/controller/CatController.kt
+++ src/main/kotlin/nl/bjornvanderlaan/livedemospringwebflux/controller/CatController.kt
@@ -1,60 +1,51 @@
-//package nl.bjornvanderlaan.livedemospringwebflux.controller
-//
-//import nl.bjornvanderlaan.livedemospringwebflux.model.CatDto
-//import nl.bjornvanderlaan.livedemospringwebflux.service.CatService
-//import org.springframework.http.ResponseEntity
-//import org.springframework.web.bind.annotation.*
-//import reactor.core.publisher.Flux
-//import reactor.core.publisher.Mono
-//
-//@RestController
-//@RequestMapping("/api/cats")
-//class CatController(private val catService: CatService) {
-//
-//    @GetMapping
-//    fun getCats(): ResponseEntity<Flux<CatDto>> =
-//        catService
-//            .getAllCats()
-//            .let {
-//                ResponseEntity.ok().body(it)
-//            }
-//
-//    @GetMapping("/{id}")
-//    fun getCat(@PathVariable id: Long): Mono<ResponseEntity<CatDto>> =
-//        catService
-//            .getCatById(id)
-//            .map { cat ->
-//                ResponseEntity.ok().body(cat)
-//            }
-//            .defaultIfEmpty(
-//                ResponseEntity.notFound().build()
-//            )
-//
-//    @PostMapping
-//    fun createCat(@RequestBody cat: CatDto): Mono<ResponseEntity<CatDto>> =
-//        catService
-//            .addNewCat(cat)
-//            .map {
-//                ResponseEntity.ok().body(cat)
-//            }
-//            .defaultIfEmpty(
-//                ResponseEntity.notFound().build()
-//            )
-//
-//    @PutMapping("/{id}")
-//    fun updateCat(@PathVariable id: Long, @RequestBody updatedCat: CatDto): Mono<ResponseEntity<CatDto>> =
-//        catService
-//            .updateExistingCat(id, updatedCat)
-//            .map { cat ->
-//                ResponseEntity.ok().body(cat)
-//            }
-//            .onErrorReturn(ResponseEntity.badRequest().build())
-//
-//    @DeleteMapping("/{id}")
-//    fun deleteCat(@PathVariable id: Long): Mono<ResponseEntity<Void>> =
-//        catService
-//            .deleteExistingCat(id)
-//            .map { ResponseEntity.noContent().build<Void>() }
-//            .onErrorReturn(ResponseEntity.notFound().build())
-//            .defaultIfEmpty(ResponseEntity.notFound().build())
-//}
+package nl.bjornvanderlaan.livedemospringwebflux.controller
+
+import kotlinx.coroutines.flow.Flow
+import nl.bjornvanderlaan.livedemospringwebflux.model.CatDto
+import nl.bjornvanderlaan.livedemospringwebflux.service.CatService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/cats")
+class CatController(private val catService: CatService) {
+
+    @GetMapping
+    fun getCats(): ResponseEntity<Flow<CatDto>> =
+        catService
+            .getAllCats()
+            .let { cats -> ResponseEntity.ok().body(cats) }
+
+
+    @GetMapping("/{id}")
+    suspend fun getCat(@PathVariable id: Long): ResponseEntity<CatDto> =
+        catService
+            .getCatById(id)
+            ?.let { cat -> ResponseEntity.ok().body(cat) }
+            ?: ResponseEntity.notFound().build()
+
+
+    @PostMapping
+    suspend fun createCat(@RequestBody cat: CatDto): ResponseEntity<CatDto> =
+        catService
+            .addNewCat(cat)
+            .let { savedCat -> ResponseEntity.ok().body(savedCat) }
+
+
+    @PutMapping("/{id}")
+    suspend fun updateCat(@PathVariable id: Long, @RequestBody updatedCat: CatDto): ResponseEntity<CatDto> =
+        if (catService.getCatById(id) != null) {
+            val savedCat = catService.updateExistingCat(id, updatedCat)
+            ResponseEntity.ok().body(savedCat)
+        } else {
+            ResponseEntity.unprocessableEntity().build()
+        }
+
+
+    @DeleteMapping("/{id}")
+    suspend fun deleteCat(@PathVariable id: Long): ResponseEntity<Void> =
+        catService.getCatById(id)?.let {
+            catService.deleteExistingCat(id)
+            ResponseEntity.noContent().build()
+        } ?: ResponseEntity.notFound().build()
+}
